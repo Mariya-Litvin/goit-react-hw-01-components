397e4a7232b842b049d4648ce7382b4d485c12b6
Mariya-Litvin <111592916+Mariya-Litvin@users.noreply.github.com>
2023-03-18 00:54:16 +0200

    update task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from './Profile';
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+import { Statistics } from './Statistics';
+import { FriendList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
     >
-      React homework template
-    </div>
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Profile>
   );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats,
+  children,
+}) => (
+  <div>
+    <div>
+      <img src={avatar} alt="User avatar" width="180" />
+      <p>{username}</p>
+      <p>{tag}</p>
+      <p>{location}</p>
+    </div>
+    <ul>
+      <li>
+        <span>Followers</span>
+        <span>{stats.followers}</span>
+      </li>
+      <li>
+        <span>Views</span>
+        <span>{stats.views}</span>
+      </li>
+      <li>
+        <span>Likes</span>
+        <span>{stats.likes}</span>
+      </li>
+    </ul>
+    {children}
+  </div>
+);
